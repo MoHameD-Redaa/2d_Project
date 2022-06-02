4c821d23b13df4194de5654ead16e0ae3b063fe9
MoHameD-Redaa <57603458+MoHameD-Redaa@users.noreply.github.com>
2022-06-02 05:44:20 +0200

    Add files via upload

--- Demo/include/Ellipse.h
+++ Demo/include/Ellipse.h
@@ -1,13 +1,14 @@
 #ifndef ELLIPSE_H_INCLUDED
 #define ELLIPSE_H_INCLUDED
 #include"Line.h"
+#include "ScreenPixels.h"
 
 void Draw4Points(HDC hdc, int xc, int yc, int x, int y, COLORREF c)
 {
-    SetPixel(hdc, xc + x, yc + y, c);
-    SetPixel(hdc, xc + x, yc - y, c);
-    SetPixel(hdc, xc - x, yc - y, c);
-    SetPixel(hdc, xc - x, yc + y, c);
+    ScreenPixels::PutPixel(hdc, xc + x, yc + y, c);
+    ScreenPixels::PutPixel(hdc, xc + x, yc - y, c);
+    ScreenPixels::PutPixel(hdc, xc - x, yc - y, c);
+    ScreenPixels::PutPixel(hdc, xc - x, yc + y, c);
 }
 
 void DirectEllipse(HDC hdc, int xc, int yc, int a, int b, COLORREF c)
